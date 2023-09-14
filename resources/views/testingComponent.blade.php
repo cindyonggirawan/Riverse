a0e12a02ea44506c2f2715389b2872aa8f90bd4d
kevicebryan <kevinbryanreligion@gmail.com>
2023-09-14 13:48:28 +0700

    add: activity cards component

---
+++ resources/views/testingComponent.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.index')
+
+@section('content')
+<div class="component-testing-page-wrapper">
+    <x-activity-card-big/>
+    <x-activity-card-horizontal/>
+    <x-activity-card/>
+</div>
+   
+@endsection
