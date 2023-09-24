06f9cc7db19d47f99f36ed506776b0536fbc6d5f
Cindy Amanda Onggirawan <70943768+cindyonggirawan@users.noreply.github.com>
2023-09-24 22:32:42 +0700

    add: Register as Sukarelawan/Fasilitator/Admin and Login Function

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Fasilitator;
+use App\Models\FasilitatorType;
+use App\Models\Role;
+use App\Models\User;
+use App\Models\Generator;
+use App\Models\Level;
+use App\Models\Sukarelawan;
+use App\Models\VerificationStatus;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view('admin.Tables.User.register', [
+            'title' => 'Register'
+        ]);
+    }
+
+    public function showSukarelawan()
+    {
+        return view('admin.Tables.Sukarelawan.register', [
+            'title' => 'Register as Sukarelawan'
+        ]);
+    }
+
+    public function storeSukarelawan(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|string|max:255|email:dns|regex:/^\S+@\S+\.\S+$/|unique:users',
+            'password' => 'required|string|min:8|max:16|regex:/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,16}$/|confirmed',
+            'name' => 'required|string|max:255|regex:/^[A-Za-z\s]+$/',
+            'gender' => 'required',
+            'dateOfBirth' => 'required|date_format:d/m/Y',
+            'nationalIdentityNumber' => 'required|string|size:16|regex:/^\d{16}$/|unique:sukarelawans'
+        ]);
+
+        $id = Generator::generateId(Sukarelawan::class);
+        $slug = Generator::generateSlug(User::class, $request->name);
+
+        User::create([
+            'id' => $id,
+            'roleId' => Role::where('name', 'Sukarelawan')->first()->id,
+            'email' => strtolower($request->email),
+            'password' => Hash::make($request->password),
+            'name' => ucwords($request->name),
+            'slug' => $slug
+        ]);
+
+        Sukarelawan::create([
+            'id' => $id,
+            'verificationStatusId' => VerificationStatus::where('name', 'Menunggu Verifikasi')->first()->id,
+            'levelId' => Level::where('name', 'Level 1')->first()->id,
+            'gender' => $request->gender,
+            'dateOfBirth' => date('Y-m-d', strtotime(str_replace('/', '-', $request->dateOfBirth))),
+            'nationalIdentityNumber' => $request->nationalIdentityNumber,
+            'experiencePoint' => 0,
+            'slug' => $slug
+        ]);
+
+        return redirect('/login')->with('success', 'Sukarelawan registration successful!');
+    }
+
+    public function showFasilitator()
+    {
+        return view('admin.Tables.Fasilitator.register', [
+            'title' => 'Register as Fasilitator',
+            'fasilitatorTypes' => FasilitatorType::all()
+        ]);
+    }
+
+    public function storeFasilitator(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|string|max:255|email:dns|regex:/^\S+@\S+\.\S+$/|unique:users',
+            'password' => 'required|string|min:8|max:16|regex:/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,16}$/|confirmed',
+            'name' => 'required|string|max:255|regex:/^[A-Za-z\s]+$/',
+            'fasilitatorTypeId' => 'required',
+            'description' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'phoneNumber' => 'required|string|min:10|max:13|regex:/^(?!62)\d{10,13}$/|unique:fasilitators'
+        ]);
+
+        $id = Generator::generateId(Fasilitator::class);
+        $slug = Generator::generateSlug(User::class, $request->name);
+
+        User::create([
+            'id' => $id,
+            'roleId' => Role::where('name', 'Fasilitator')->first()->id,
+            'email' => strtolower($request->email),
+            'password' => Hash::make($request->password),
+            'name' => ucwords($request->name),
+            'slug' => $slug
+        ]);
+
+        Fasilitator::create([
+            'id' => $id,
+            'verificationStatusId' => VerificationStatus::where('name', 'Menunggu Verifikasi')->first()->id,
+            'fasilitatorTypeId' => $request->fasilitatorTypeId,
+            'description' => $request->description,
+            'address' => $request->address,
+            'phoneNumber' => $request->phoneNumber,
+            'slug' => $slug
+        ]);
+
+        return redirect('/login')->with('success', 'Fasilitator registration successful!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,19 @@
 <?php
 
+use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\ActivityStatusController;
+use App\Http\Controllers\BenefitController;
+use App\Http\Controllers\ExperiencePointStatusController;
+use App\Http\Controllers\FasilitatorController;
+use App\Http\Controllers\FasilitatorTypeController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\SukarelawanController;
+use App\Http\Controllers\SukarelawanStatusController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\VerificationStatusController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +27,80 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+// Route::get('/component', function () {
+//     return view('testingComponent');
+// });
+
+// Route::get('/activities', [ActivityController::class, 'index']);
+
+// Route::get('/activities/{slug}', [ActivityController::class, 'show']);
+
 Route::get('/', function () {
-    return view('home');
+    return view('admin.layouts.main', [
+        'title' => 'Home'
+    ]);
 });
 
-Route::get('/component', function () {
-    return view('testingComponent');
-});
+Route::get('/roles', [RoleController::class, 'index']);
+
+Route::get('/roles/{role:slug}', [RoleController::class, 'show']);
+
+Route::get('/fasilitator-types', [FasilitatorTypeController::class, 'index']);
+
+Route::get('/fasilitator-types/{fasilitatorType:slug}', [FasilitatorTypeController::class, 'show']);
+
+Route::get('/verification-statuses', [VerificationStatusController::class, 'index']);
+
+Route::get('/verification-statuses/{verificationStatus:slug}', [VerificationStatusController::class, 'show']);
+
+Route::get('/activity-statuses', [ActivityStatusController::class, 'index']);
+
+Route::get('/activity-statuses/{activityStatus:slug}', [ActivityStatusController::class, 'show']);
+
+Route::get('/sukarelawan-statuses', [SukarelawanStatusController::class, 'index']);
+
+Route::get('/sukarelawan-statuses/{sukarelawanStatus:slug}', [SukarelawanStatusController::class, 'show']);
+
+Route::get('/experience-point-statuses', [ExperiencePointStatusController::class, 'index']);
+
+Route::get('/experience-point-statuses/{experiencePointStatus:slug}', [ExperiencePointStatusController::class, 'show']);
+
+Route::get('/levels', [LevelController::class, 'index']);
+
+Route::get('/levels/{level:slug}', [LevelController::class, 'show']);
+
+Route::get('/benefits', [BenefitController::class, 'index']);
+
+Route::get('/benefits/{benefit:slug}', [BenefitController::class, 'show']);
+
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+
+Route::get('/register/sukarelawan', [RegisterController::class, 'showSukarelawan']);
+
+Route::post('/register/sukarelawan', [RegisterController::class, 'storeSukarelawan']);
+
+Route::get('/register/fasilitator', [RegisterController::class, 'showFasilitator']);
+
+Route::post('/register/fasilitator', [RegisterController::class, 'storeFasilitator']);
+
+Route::get('/users', [UserController::class, 'index']);
+
+Route::get('/users/{user:slug}', [UserController::class, 'show']);
+
+Route::get('/sukarelawans', [SukarelawanController::class, 'index']);
+
+Route::get('/sukarelawans/{sukarelawan:slug}', [SukarelawanController::class, 'show']);
+
+Route::get('/fasilitators', [FasilitatorController::class, 'index']);
+
+Route::get('/fasilitators/{fasilitator:slug}', [FasilitatorController::class, 'show']);
+
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+Route::post('/logout', [LoginController::class, 'logout']);
