d74bbb89a508ad5d210bc1e3c0b0e6ec7a330ec3
Kevin Bryan <70892991+kevicebryan@users.noreply.github.com>
2023-12-08 21:21:23 +0700

    add: sukarelawan manage dashboard (#19)

--- routes/web.php
+++ routes/web.php
@@ -111,6 +111,8 @@ Route::get('/sukarelawans', [SukarelawanController::class, 'index']);
 
 Route::get('/sukarelawans/{sukarelawan:slug}', [SukarelawanController::class, 'publicShow']);
 
+Route::get('/sukarelawans/{sukarelawan:slug}/manage', [SukarelawanController::class, 'manage']);
+
 Route::get('/fasilitators', [FasilitatorController::class, 'index']);
 
 Route::get('/fasilitators/{fasilitator:slug}', [FasilitatorController::class, 'publicShow']);
