85dd0cb7a4246b037320a3479722ad248d49376a
elliot.arifin <elliot.arifin@gdn-commerce.com>
2023-12-11 00:50:49 +0700

    improve navbar css for finalize attendance page

--- resources/views/fasilitator/finalizeAttendance/tabbar/attendanceVerification.blade.php
+++ resources/views/fasilitator/finalizeAttendance/tabbar/attendanceVerification.blade.php
@@ -1,16 +1,10 @@
-@include('layout.useBootstrap')
-
-<div class="card-header p-0 border-bottom-0">
-    <ul class="nav nav-tabs" id="custom-tabs-four-tab" role="tablist">
-        <li class="nav-item">
-            <a class="nav-link {{ $title === 'Unapproved Sukarelawan' ? 'active' : '' }}"
-                href="/{{ $activity->slug }}/waiting-for-verification/attendance">Yang Menunggu
-                Verifikasi</a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link {{ $title === 'Approved Sukarelawan' ? 'active' : '' }}"
-                href="/{{ $activity->slug }}/attended/attendance">Yang Diverifikasi</a>
-        </li>
-    </ul>
-</div>
+<nav class="row tab-menu">
+    <div class="{{ $title === 'Unapproved Sukarelawan' ? 'tab-selected' : 'tab-unselected' }}">
+        <a class="tab-link {{ $title === 'Unapproved Sukarelawan' ? 'active' : '' }}"
+        href="/{{ $activity->slug }}/waiting-for-verification/attendance">Yang Menunggu Verifikasi</a>
+    </div>
+    <div class="{{ $title === 'Approved Sukarelawan' ? 'tab-selected' : 'tab-unselected' }}">
+        <a class="tab-link {{ $title === 'Approved Sukarelawan' ? 'active' : '' }}"
+        href="/{{ $activity->slug }}/attended/attendance">Yang Diverifikasi</a>
+    </div>
+</nav>
